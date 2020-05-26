c2dfc3ed15570b250876528cc9d57b24b334982a
kurocraft0312 <kurocraft0312@gmail.com>
2020-05-27 01:32:47 +0900

    WIP:レイアウト設計中

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,63 @@
 <template>
+  <!-- div消しても動くか今度テストする -->
   <div>
-
+      <header class="header">
+        <h1 class="header_title">
+          <img src="/" class="header_title_logo" alt="タイトルロゴ">
+        </h1>
+        <address class="header_tel">
+          <a href="tel:08089186908" class="header_telnumber">080-8918-6908</a>
+        </address>
+      </header>
+      <div class="">
+        <!-- この辺グリッドレイアウトを使う -->
+        <!-- キャッチコピー -->
+        <!-- 権威付け -->
+        <!-- メインビジュアル -->
+      </div>
+      <button class="cv_btn">商品LPの制作を相談する[無料]</button>
+      <!-- 導入・きっかけ -->
+      <!-- ベネフィット -->
+      <button class="cv_btn">商品LPの制作を相談する[無料]</button>
+      <!-- 商品・サービス説明 -->
+      <button class="cv_btn">商品LPの制作を相談する[無料]</button>
+      <!-- お客様の声・口コミ -->
+      <button class="cv_btn">商品LPの制作を相談する[無料]</button>
+      <!-- FAQ -->
+      <button class="cv_btn">商品LPの制作を相談する[無料]</button>
+      <footer class="footer">
+        <small>&copy; 2020 HP制作ブランド「Ratpost_hp」</small>
+      </footer>
   </div>
 </template>
 <script>
 
 </script>
 <style lang="stylus" scoped>
-
+  a {
+    text-decoration: none;
+  }
+  .header {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  }
+  .header_telnumber {
+    color: #000000;
+    font-weight: bold;
+  }
+  .cv_btn {
+    border: 0;
+    background-color: #FF3300;
+    border-radius: 4px;
+    color: #ffffff;
+    font-weight: bold;
+    padding: 8px;
+  }
+  .footer {
+    background-color: #2e2e2e;
+    color: #ffffff;
+    padding: 8px;
+    text-align: center;
+  }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -48,7 +48,7 @@ export default {
   text-align: center;
 }
 
-.title {
+/* .title {
   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: block;
@@ -56,7 +56,7 @@ export default {
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
-}
+} */
 
 .subtitle {
   font-weight: 300;
